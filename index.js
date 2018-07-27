60a5e191576362225428c9ce25f1950bf13d2e14
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 14:04:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+  return Math.abs(someValue-42)
+}
+
+function distanceFromHqInFeet(someValue){
+  return distanceFromHqInBlocks(someValue)*264
+}
+
+function distanceTravelledInFeet(startBlock,endBlock){
+  return Math.abs(endBlock-startBlock)*264
+}
+
+function calculatesFarePrice(start,destination){
+  var totalDistance = distanceTravelledInFeet(start,destination)
+  totalDistance = totalDistance-400
+  var farePrice = 0
+  if (totalDistance>400 && totalDistance<2000){
+    farePrice = (2*totalDistance)/100
+    return farePrice
+  }
+  else if (totalDistance>2000 && totalDistance<2500){
+    return "25 dollars"
+  }
+  else {
+    return "cannot travel that far!"
+  }
+}
